f33a906c92366458902f9907fbb767fc6bd55d9b
velvet_vixen <hei.karenandriantasy@gmail.com>
2023-12-04 19:43:49 +0300

    feat: add constructor and getter setter for account & account repository : method addAccount added

--- src/model/Account.java
+++ src/model/Account.java
@@ -8,4 +8,62 @@ public class Account {
     private String currencyId;
     private String transactionId;
 
+    public Account(String id, String accountName, String accountType, float balance, String currencyId, String transactionId) {
+        this.id = id;
+        this.accountName = accountName;
+        this.accountType = accountType;
+        this.balance = balance;
+        this.currencyId = currencyId;
+        this.transactionId = transactionId;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public String getCurrencyId() {
+        return currencyId;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setAccountName(String accountName) {
+        this.accountName = accountName;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public void setCurrencyId(String currencyId) {
+        this.currencyId = currencyId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
 }
+
+
---
+++ src/repository/AccountRepository.java
@@ -0,0 +1,33 @@
+package repository;
+
+import model.Account;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class AccountRepository {
+    private static final String INSERT_ACCOUNT_SQL = "INSERT INTO account (id, account_name, account_type, balance, currency_id, transaction_id) VALUES (?, ?, ?, ?, ?, ?)";
+    private static final String SELECT_ALL_ACCOUNTS_SQL = "SELECT * FROM account";
+    private static final String SELECT_ACCOUNT_BY_ID_SQL = "SELECT * FROM account WHERE id = ?";
+    private static final String UPDATE_ACCOUNT_SQL = "UPDATE account SET account_name = ?, account_type = ?, balance = ?, currency_id = ?, transaction_id = ? WHERE id = ?";
+
+    private Connection connection;
+
+    private AccountRepository(Connection connection){
+        this.connection = connection;
+    }
+    public void addAccount(Account account){
+        try(PreparedStatement preparedStatement = connection.prepareStatement(INSERT_ACCOUNT_SQL)){
+            preparedStatement.setString(1, account.getId());
+            preparedStatement.setString(2, account.getAccountName());
+            preparedStatement.setString(3, account.getAccountType());
+            preparedStatement.setFloat(4,account.getBalance());
+            preparedStatement.setString(5,account.getCurrencyId());
+            preparedStatement.setString(6, account.getTransactionId());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
